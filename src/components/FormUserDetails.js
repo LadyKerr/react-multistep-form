2c2c43b75087d4258986976c273c58a7eca54827
LadyKerr <kadiekerr@gmail.com>
2019-06-01 17:03:41 -0400

    class UserForm created

---
+++ src/components/FormUserDetails.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class FormUserDetails extends React.Component {
+  constructor() {
+    super();
+    render () {
+      return (
+        <div>
+          
+        </div>
+      );
+    }
+  }
+}
+
+export default FormUserDetails;
---
+++ src/components/UserForm.js
@@ -0,0 +1,64 @@
+import React from "react";
+import FormUserDetails from "./FormUserDetails";
+
+class UserForm extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      step: 1,
+      firstName: " ",
+      lastName: " ",
+      email: " ",
+      occupation: " ",
+      city: " ",
+      bio: " ",
+    }
+
+    //proceed to next step
+    nextStep = () => {
+    const { step } = this.state;
+    this.setState ({
+      step: step + 1
+      });
+    }
+
+    //return to previous step
+    prevStep = () => {
+      const { step } = this.state;
+      this.setState ({
+        step: step - 1
+        });
+      }
+
+    //handlefieldschange
+    handleChange = input => e => {
+      this.setState ({input: e.target.value})
+    }
+
+    render() {
+      const { step } = this.state;
+      const { firstName, lastName, email, occupation, city, bio } = this.state;
+      const values = { firstName, lastName, email, occupation, city, bio }
+
+      switch(step) {
+        case 1:
+          return (
+            <FormUserDetails 
+            nextStep={this.nextStep}
+            handleChange={this.handleChange}
+            values={values}
+            />
+            )
+        case 2:
+          return <h1>FormPersonalDetails</h1>
+        case 3:
+          return <h2>Confirm</h2>
+        case 4:
+          return <h2>Success!</h2>
+      }
+    }
+  }
+}
+
+export default UserForm;
